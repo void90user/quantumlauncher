b66617c87c648cf4545d26a51c255621c8ea12e2
Void98 <shadow_pie99@proton.me>
2026-03-18 23:22:51 +0100

    Started work on linking

--- crates/ql_core/src/symbolic_link.rs
+++ crates/ql_core/src/symbolic_link.rs
@@ -1,47 +1,32 @@
-use symlink_rs;
-use std::fs::metadata;
-use symlink_rs::{symlink_file, symlink_dir};
+use std::fs;
 use std::path::Path;
+use serde_json::Value::String;
+use crate::LAUNCHER_DIR;
 
-pub async fn create_syslink<P: AsRef<Path>>(targets: &[(P, P)]) -> std::io::Result<()> {
-
-    for (target, link) in targets {
-        let target_path = target.as_ref();
-        let link_path = link.as_ref();
-        let metadata = metadata(target_path)?;
-
-        if metadata.is_dir() {
-            symlink_dir(target_path, link_path)?;
-        } else {
-            symlink_file(target_path, link_path)?;
-        }
+async fn create_hard_links(links: Vec<(&str, &str)>) -> std::io::Result<()> {
+    for (from, to) in links {
+        fs::hard_link(from, to)?;
     }
-
     Ok(())
-
 }
 
 
-// FOR FRONTEND
+pub async fn file_linking() {
 
+    let file_type;
+    //TODO: ADD THE FILE TYPE LIKE: resourcepack, config ect.
+    let file_names;
+    //TODO: ADD THE FILE NAMES
 
+    let global_source_path = LAUNCHER_DIR.join("global_resources");
+    let sources: Vec<&str> = vec![] ;
+    let destinations: Vec<&str> = vec![];
 
+    sources.join("as");
+    destinations.join("as");
 
+    let paths: Vec<(&str, &str)> = sources.into_iter().zip(destinations.into_iter()).collect();
 
+    create_hard_links(paths).await.expect("Unable to create links");
 
-
-/*
-files_to_link.push(("config.toml", "cfg_link.toml"));
-files_to_link.push(("data_dir", "link_to_data"));
-
-create_symlinks(&files_to_link)?;
-let mut files_to_link = Vec::new();
-
-
-
-let file_paths = vec!["src/file1.txt", "src/file2.txt"];
-let link_paths = vec!["links/l1.txt", "links/l2.txt"];
-
-let targets: Vec<_> = file_paths.into_iter().zip(link_paths.into_iter()).collect();
-
- */
+}
