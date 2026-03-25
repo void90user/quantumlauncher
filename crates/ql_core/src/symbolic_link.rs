7d629cbebf67d99d4a63909469afadd7f8b681ae
Void98 <shadow_pie99@proton.me>
2026-03-25 12:49:41 +0100

    Linking for my part mostly done

--- crates/ql_core/src/symbolic_link.rs
+++ crates/ql_core/src/symbolic_link.rs
@@ -1,6 +1,5 @@
 use std::fs;
 use std::path::Path;
-use serde_json::Value::String;
 use crate::LAUNCHER_DIR;
 
 async fn create_hard_links(links: Vec<(&str, &str)>) -> std::io::Result<()> {
@@ -13,20 +12,34 @@ async fn create_hard_links(links: Vec<(&str, &str)>) -> std::io::Result<()> {
 
 pub async fn file_linking() {
 
+    /*
+    TODO: This might help you do the frontend
+
     let file_type;
-    //TODO: ADD THE FILE TYPE LIKE: resourcepack, config ect.
+    //TODO: ADD THE FILE TYPE LIKE: resourcepack, config, shaderpack .
     let file_names;
     //TODO: ADD THE FILE NAMES
 
-    let global_source_path = LAUNCHER_DIR.join("global_resources");
+    let selected_global_source_path = LAUNCHER_DIR.join("global_resources").join(file_type);
+    let instance_path:Path = "current instance path";
+    // TODO: add the selected instance here ^^^^^^^
+    let instance_resource_path;
+
+    if file_type == "config" { instance_resource_path = instance_path.join(".minecraft") }
+    else { instance_resource_path = instance_path.join(".minecraft").join(file_type) }
+
+     */
+
     let sources: Vec<&str> = vec![] ;
     let destinations: Vec<&str> = vec![];
+    // TODO: THESE NEED TO BE ABSOLUTE PATHS LIKE: sources -> ("SOURCE/FILE/PATH.zip", "SOURCE/FILE/PATH2.zip") dest -> ("DEST/FILE/PATH.zip", "DEST/FILE/PATH2.zip")
 
-    sources.join("as");
-    destinations.join("as");
+    let links: Vec<(&str, &str)> = sources.iter()
+        .zip(destinations.iter())
+        .map(|(&src, &dst)| (src, dst))
+        .collect();
 
-    let paths: Vec<(&str, &str)> = sources.into_iter().zip(destinations.into_iter()).collect();
 
-    create_hard_links(paths).await.expect("Unable to create links");
+    create_hard_links(links).await.expect("Unable to create links");
 
 }
